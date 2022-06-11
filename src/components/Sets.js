9eba754b2b501d20a598e7e5ed5fb17440fdebf3
Robinborg <robin.borgatrom@gmail.com>
2022-06-11 15:29:23 +0300

    Sets workout list now only shows the update to the currently selected exercise

--- src/components/Sets.js
+++ src/components/Sets.js
@@ -1,22 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import CurrentSet from './CurrentSet';
 
 const Sets = props => {
 	const [currentSetCount, setCurrentSetCount] = useState(0);
-	let first = 1;
-	let second = 0;
-	let third = 0;
-	let fourth = 0;
-	let fifth = 0;
+	const first = useRef(true);
+	const second = useRef(false);
+	const third = useRef(false);
+	const fourth = useRef(false);
+	const fifth = useRef(false);
+
+	useEffect(() => {
+		if (currentSetCount === 5) {
+			if (!second.current && first.current) {
+				second.current = true;
+				first.current = false;
+			} else if (!third.current && second.current) {
+				third.current = true;
+				second.current = false;
+			} else if (!fourth.current && third.current) {
+				fourth.current = true;
+				third.current = false;
+			} else if (!fifth.current && fourth.current) {
+				fifth.current = true;
+				fourth.current = false;
+			} else {
+				alert("done working out");
+		};
+		}
+	}, [currentSetCount]);
 
 	return (
 		<div>
 			<ol>
-				<li>{ props.setOne } { (first) ?  currentSetCount : 0  } / 5</li>
-				<li>{ props.setTwo } { (second) ? currentSetCount : 0} / 5</li>
-				<li>{ props.setThree } { (third) ? currentSetCount : 0} / 5</li>
-				<li>{ props.setFour } { (fourth) ? currentSetCount : 0 } / 5</li>
-				<li>{ props.setFive } { (fifth) ? currentSetCount : 0} / 5</li>
+				<li>{ props.setOne } { (first.current) ?  currentSetCount : 0  } / 5</li>
+				<li>{ props.setTwo } { (second.current) ? currentSetCount : 0 } / 5</li>
+				<li>{ props.setThree } { (third.current) ? currentSetCount : 0 } / 5</li>
+				<li>{ props.setFour } { (fourth.current) ? currentSetCount : 0 } / 5</li>
+				<li>{ props.setFive } { (fifth.current) ? currentSetCount : 0 } / 5</li>
 			</ol>
 			<div>
 				<CurrentSet 
