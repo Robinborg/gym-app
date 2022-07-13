ad37a04ba17a9cd8ba4b18b71314ec95f5934039
Robinborg <robin.borgatrom@gmail.com>
2022-07-13 16:01:20 +0300

    New approach to design of exerciseList and showing current exercise - Removed CurrentSet from the application - TODO: add a highlighter to the current exercise in the exerciseList

--- src/app/App.js
+++ src/app/App.js
@@ -4,7 +4,6 @@ import { useForm } from 'react-hook-form';
 import MainTimer from '../features/timers/MainTimer';
 import Timer from '../features/timers/Timer';
 import Sets from '../features/sets/Sets';
-import CurrentSet from '../features/sets/CurrentSet';
 import ExerciseEntryForm from './ExerciseEntryForm';
 import './style.css';
 
@@ -29,7 +28,6 @@ const App = () => {
 			  	</button>
 			</div>
 			<Sets />
-			<CurrentSet />
 			<Timer />
 		</div>
 	);
