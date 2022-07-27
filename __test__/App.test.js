7e825dee6417086d2f7d4152fd8ff80a39e69a45
Robinborg <robin.borgatrom@gmail.com>
2022-07-27 13:04:38 +0300

    Testing Rest time buttons and Redux - Initial State is set to 03:00 - Pressing button for 04:00 timed break and check that it updates - Pressing button for 05:00 timed break and check that it updates

--- __test__/App.test.js
+++ __test__/App.test.js
@@ -4,9 +4,15 @@
 
 import React from 'react';
 import { renderWithProviders } from '.././utils/test-utils';
+import { fireEvent, screen } from '@testing-library/react';
 import App from '.././src/app/App';
 
 
 test("renders without crashing", () => {
-	renderWithProviders(<App/>)
+	renderWithProviders(<App/>);
+	expect(screen.getByText(/Rest Time: 03:00/i)).toBeInTheDocument();
+	fireEvent.click(screen.getByRole('button', { name: /04:00/i}));
+	expect(screen.getByText(/Rest Time: 04:00/i)).toBeInTheDocument();
+	fireEvent.click(screen.getByRole('button', { name: /05:00/i}));
+	expect(screen.getByText(/Rest Time: 05:00/i)).toBeInTheDocument();
 })
--- jest.config.js
+++ jest.config.js
@@ -12,4 +12,5 @@ module.exports = {
 	],
 	transformIgnorePatterns: ['<rootDir>/node_modules/'],
 	verbose: true,
+	setupFilesAfterEnv: ['<rootDir>/jest-setup.js']
 };
